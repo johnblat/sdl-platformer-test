b42475005964356e3d22c2be89c6c41fe32c5964
John Blat <johnblat64@protonmail.com>
2022-01-20 17:40:55 -0500

    input

--- include/input.h
+++ include/input.h
@@ -3,14 +3,58 @@
 
 #include "jbInts.h"
 #include <SDL2/SDL.h>
+#include <string>
+#include "flecs.h"
 
-struct KeyMap {
-    char name[16];
-    SDL_Scancode sdlScancode;
-};
+/**
+ * @brief Contains the current key states of the game
+ * Ought to be updated every frame with SDL_PollEvent
+ * 
+ */
+extern u8 *gKeyStates;
+
+
 
 struct KeyboardState{
     u8 *keyStates;
 };
+
+
+typedef enum InputState {
+    INPUT_IS_JUST_PRESSED,
+    INPUT_IS_PRESSED,
+    INPUT_IS_JUST_RELEASED,
+    INPUT_IS_NOT_PRESSED
+};
+
+
+typedef struct InputButtonState {
+    std::string name;
+    SDL_Scancode sdlScancode;
+    InputState currentInputState;
+    InputState previousInputState;
+};
+
+
+typedef struct Input Input;
+struct Input {
+    InputButtonState *buttonStates;
+    u64 numButtomStates;
+};
+
+
+/**
+ * UTILS
+ */
+bool isJustPressed(Input input, std::string buttonName);
+bool isPressed(Input input, std::string buttonName);
+bool isJustReleased(Input input, std::string buttonName);
+
+
+/**
+ * SYSTEMS
+ */
+void inputUpdateSystem(flecs::iter &it, Input *inputs);
+
 
 #endif
