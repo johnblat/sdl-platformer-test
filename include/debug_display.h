92026d2184a3e436dc4a5fe828440f133d80afa3
John Blat <johnblat64@protonmail.com>
2022-03-09 10:17:26 -0500

    add includes used

--- include/debug_display.h
+++ include/debug_display.h
@@ -1,6 +1,9 @@
 #ifndef dbg_display_h
 #define dbg_display_h
 
+#include "ray2d.h"
+#include <vector>
+
 void renderRectangularObjectsSystem(flecs::iter &it, Position *positions, SolidRect *rectObjects);
 void renderRay2dCollectionsSystem(flecs::iter &it, Position *positions, std::vector<Ray2d> *ray2dCollections);
 
