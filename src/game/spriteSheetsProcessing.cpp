4eb2c150b457b0c4db12d19761afc9d35a239ee6
John Blat <johnblat64@protonmail.com>
2022-05-12 13:30:20 -0400

    remove comments

--- src/game/spriteSheetsProcessing.cpp
+++ src/game/spriteSheetsProcessing.cpp
@@ -11,18 +11,7 @@
 
 u32 createSpriteSheet(const char *filename, size_t numCellRows, size_t numCellCols, const std::string name){
     SDL_Surface *imgSurface = IMG_Load(filename);
-    Uint32 alphaColor = 0x234c73;
 
-    // for(int y = 0; y < imgSurface->h; y++){
-    //     for(int x = 0; x < imgSurface->w; x++){
-    //         SDL_PixelFormat format = *(imgSurface->format);
-    //         Uint8 *pixels = (Uint8 *)imgSurface->pixels;
-    //         Uint8 pixel = pixels[y*imgSurface->w + x];
-    //         if(pixel == alphaColor){
-    //             pixels[y*imgSurface->h + x] = 0x0;
-    //         }
-    //     }
-    // }
     SDL_Texture *texture = SDL_CreateTextureFromSurface(gRenderer, imgSurface);
     SpriteSheet spriteSheet;
     spriteSheet.texture = texture;
